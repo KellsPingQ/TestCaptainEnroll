3c439f0e163bba65240ece5fe35846ebd7813cf3
Kells_P <kellsping@gmail.com>
2023-10-17 15:40:46 +0800

    Update.
    Function :
    1. Start up "TestManager.exe", then close itself.

--- Program.cs
+++ Program.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace TestCaptainEnroll
 {
@@ -10,8 +13,19 @@ namespace TestCaptainEnroll
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello TestCaptainEnroll");
-            Console.ReadLine();
+            /* relative path of "TestManager.exe" */
+            string path = "..\\..\\..\\TestManager\\bin\\Debug\\TestManager.exe";
+
+            Console.WriteLine("Hello TestCaptainEnroll.");
+
+            /* get current path */
+            // Console.WriteLine(System.IO.Path.GetDirectoryName(Assembly.GetEntryAssembly().Location));
+
+            /* start up "TestManager.exe" */
+            Process.Start(path);
+            
+            /* hold the console, if not, "TestCaptainEnroll.exe" will close itself */
+            // Console.ReadLine();
         }
     }
 }
