4bbe8a4fba542124753ccd312c36d9c8c065fd52
Kells_P <kellsping@gmail.com>
2023-10-17 15:19:06 +0800

    Update. Print text and hold before exit the console.

--- Program.cs
+++ Program.cs
@@ -10,6 +10,8 @@ namespace TestCaptainEnroll
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello TestCaptainEnroll");
+            Console.ReadLine();
         }
     }
 }
